20efb3a49bd58ae91ad8a054ca2853dcdbec38d0
Sarat Chandra Pasumarthy <p.saratchandra4357@gmail.com>
2016-05-24 17:28:54 +0530

    breakdown
    step-by-step breakdown of hashing

--- hash.py
+++ hash.py
@@ -3,7 +3,17 @@ def given_hash(test_string):
     h = 7
     letters = "acdegilmnoprstuw"
     for i in range(len(test_string)):
+        val = h * 37
+        index = letters.index(test_string[i])
+        print h, val, index
         h = (h * 37 + letters.index(test_string[i]))
     return h
 
+
+def reverse_hash(test_number):
+    return result
+
 print given_hash("leepdag")  # Given example: leepadg => 680131659347
+
+# TODO implement reverse hash
+# Task reverse of 930846109532517 = ?
