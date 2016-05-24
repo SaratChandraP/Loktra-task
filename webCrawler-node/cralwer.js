62b50666bcd31abee033deff0ab844774d45119b
Sarat Chandra Pasumarthy <p.saratchandra4357@gmail.com>
2016-05-24 18:24:51 +0530

    query 1
    query 1 functionality ready

--- webCrawler-node/cralwer.js
+++ webCrawler-node/cralwer.js
@@ -21,10 +21,29 @@ if (process.argv.length == 4){
     query2();
 }
 
+// Query 1: Find number of search results for a given keyword
 function query1(){
+    console.log("Searching for keyword: "+process.argv[2]);
+    // Making user input keyword URL compatible
+    var keyword = process.argv[2].replace(/\s/g,"+");
 
+    request('http://www.shopping.com/products?KW=' + keyword, function (error, response, body) {
+        if (error){
+            throw Error(error);
+        }
+        if (response.statusCode !== 200) {
+            return console.log("Returned response code = " + response.statusCode);
+        }
+        if (!error && response.statusCode === 200){
+            var $ = cheerio.load(body);
+            var result = $('span.numTotalResults').text();
+            matched = result.match(/of\s(\d*\+?)\n/);
+            console.log("Number of results returned = " + matched[1]);
+        }
+    })
 }
 
+// Query 2: Find all results for a given keyword on a specified page
 function query2(){
-    
+
 }
