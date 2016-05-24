48b4a7a11da84c01275e7066f33e21e3cf94ccc3
Sarat Chandra Pasumarthy <p.saratchandra4357@gmail.com>
2016-05-24 17:55:57 +0530

    clean up
    formatting and clean up of code

--- hash.py
+++ hash.py
@@ -11,20 +11,22 @@ def given_hash(test_string):
     return h
 
 
+# Function for reversing hash
 def reverse_hash(test_number):
     h = 7
     letters = "acdegilmnoprstuw"
     result = ''
     # Work backwards with the test_number to get the input string
     while test_number > (7*37):
-        index = test_number % 37    # Gives index of letter in 'letters' string
-        result += letters[index]    # Get that letter and append to result
-        # print test_number, index    # For verification purposes
+        index = test_number % 37                    # Gives index of letter in 'letters' string
+        result += letters[index]                    # Get that letter and append to result
+        # print test_number, index                  # For verification purposes
         test_number = (test_number - index)/37
-    hashed_string = result[::-1]   # reversing produces string backwards
-    return hashed_string
+    hashed_string = result[::-1]                    # reversing produces string backwards
+    return hashed_string                               # Or can simply use return result[::-1]
 
-print given_hash("leepadg")  # Given example: leepadg => 680131659347
+print given_hash("leepadg")         # Given example: leepadg => 680131659347
 print reverse_hash(680131659347)    # Should get leepadg
+
 # Reverse of 930846109532517 = ?
 print reverse_hash(930846109532517) # Answer is lawnmower
