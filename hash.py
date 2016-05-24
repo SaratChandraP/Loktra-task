87b6f30f660c51b075fe2b67ddff87320d0aebc2
Sarat Chandra Pasumarthy <p.saratchandra4357@gmail.com>
2016-05-24 17:45:13 +0530

    done
    implemented and tested reverse hash

--- hash.py
+++ hash.py
@@ -4,9 +4,9 @@ def given_hash(test_string):
     letters = "acdegilmnoprstuw"
     for i in range(len(test_string)):
         # Breakdown for verification and analysis purposes
-        val = h * 37
-        index = letters.index(test_string[i])
-        print h, val, index
+        # val = h * 37
+        # index = letters.index(test_string[i])
+        # print h, val, index
         h = (h * 37 + letters.index(test_string[i]))
     return h
 
@@ -19,12 +19,12 @@ def reverse_hash(test_number):
     while test_number > (7*37):
         index = test_number % 37    # Gives index of letter in 'letters' string
         result += letters[index]    # Get that letter and append to result
-        print test_number, index    # For verification purposes
+        # print test_number, index    # For verification purposes
         test_number = (test_number - index)/37
     hashed_string = result[::-1]   # reversing produces string backwards
     return hashed_string
 
 print given_hash("leepadg")  # Given example: leepadg => 680131659347
-
-# Reverse of 930846109532517 = ?
 print reverse_hash(680131659347)    # Should get leepadg
+# Reverse of 930846109532517 = ?
+print reverse_hash(930846109532517) # Answer is lawnmower
