e83e18cfd8d066e3801092fc1ff29cbd3ad51c2c
Sarat Chandra Pasumarthy <p.saratchandra4357@gmail.com>
2016-05-24 17:34:42 +0530

    implementation
    initial implementation of reverse hash using breakdown of hashing
    function

--- hash.py
+++ hash.py
@@ -3,6 +3,7 @@ def given_hash(test_string):
     h = 7
     letters = "acdegilmnoprstuw"
     for i in range(len(test_string)):
+        # Breakdown for verification and analysis purposes
         val = h * 37
         index = letters.index(test_string[i])
         print h, val, index
@@ -11,9 +12,17 @@ def given_hash(test_string):
 
 
 def reverse_hash(test_number):
+    h = 7
+    letters = "acdegilmnoprstuw"
+    result = ''
+    # Work backwards with the test_number to get the input string
+    while test_number >= 0:
+        index = test_number % 37    # Gives index of letter in 'letters' string
+        result += letters[index]    # Get that letter and append to result
+        print test_number, index    # For verification purposes
+        test_number = (test_number - index)/37
     return result
 
-print given_hash("leepdag")  # Given example: leepadg => 680131659347
+print given_hash("leepadg")  # Given example: leepadg => 680131659347
 
-# TODO implement reverse hash
-# Task reverse of 930846109532517 = ?
+# Reverse of 930846109532517 = ?
